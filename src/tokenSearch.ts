e38e061742f0408f70c25a6772c6af446fc11a05
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-04-26 11:55:22 +0200

    Add search by form, morpheme segmentation and gloss, translation and part of speech

--- src/tokenSearch.ts
+++ src/tokenSearch.ts
@@ -2,14 +2,20 @@ import {Token} from '@pictalk-speech-made-easy/conllu-parser';
 import {SearchQuery, matchesQuery} from './search/searchQuery';
 import {defaultLookupConfig} from './search/lookupConfig';
 
-type TokenQueryField = 'Gender' | 'Number'| 'Case'| 'Definite' | 'VerbForm' |
-  'Mood' | 'Tense' | 'Person' | 'Voice' | 'Degree' | 'PronType';
+type TokenQueryField = 'form' | 'MSeg' | 'MGloss' | 'Gloss' | 'upos' | 'Gender' | 'Number'| 'Case'
+| 'Definite' | 'VerbForm' | 'Mood' | 'Tense' | 'Person' | 'Voice' | 'Degree' | 'PronType';
 
 export const tokenQueryFields: TokenQueryField[] = [
-  'Gender', 'Number', 'Case', 'Definite', 'VerbForm',
+  'form', 'MSeg', 'MGloss', 'Gloss', 'upos', 'Gender', 'Number', 'Case', 'Definite', 'VerbForm',
   'Mood', 'Tense', 'Person', 'Voice', 'Degree', 'PronType'
 ]
 
 export function tokenMatchesQuery(token: Token, query: SearchQuery<TokenQueryField>): boolean {
-  return token.feats !== '_' && matchesQuery(token.feats, query, defaultLookupConfig);
+  const {feats, misc} = token;
+  const searchableObject = {
+    ...token,
+    ...(feats !== '_' ? feats : {}),
+    ...(misc !== '_' ? misc : {}),
+  }
+  return token.feats !== '_' && matchesQuery(searchableObject, query, defaultLookupConfig);
 }
