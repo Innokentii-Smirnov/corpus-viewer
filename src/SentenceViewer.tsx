f270f8603dbb375540505d9bec94e9fc98ad9912
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-04-26 10:20:46 +0200

    Display sentence translations

--- src/SentenceViewer.tsx
+++ src/SentenceViewer.tsx
@@ -8,13 +8,19 @@ interface IProps {
 
 export function SentenceViewer({sentence}: IProps) {
   const tokens = groupTokens(sentence);
+  const {translation} = sentence.metadata;
   return (
+    <div>
     <div className="sentence-viewer">
       {tokens.map((token: MultiwordToken | Token, index: number) =>
         isMultiword(token)
         ? <MultiwordTokenViewer multiwordToken={token} key={index}/>
         : <MultiwordTokenViewer multiwordToken={{form: token.form, words: [token]}} key={index}/>
       )}
+    </div>
+    <div className="translation">
+      {translation}
+    </div>
     </div>
   );
 }
